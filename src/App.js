575e98563df07977576e7b91190cd2b37525bb28
Ubuntu <ubuntu@ip-172-31-25-147.us-east-2.compute.internal>
2019-12-12 16:56:13 +0000

    try API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
     class App extends Component {
-      render() {
-        return (
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Steve Jobs</h5>
-              <h6 class="card-subtitle mb-2 text-muted">steve@apple.com</h6>
-              <p class="card-text">Stay Hungry, Stay Foolish</p>
-            </div>
-          </div>
-        );
+      
+      state = {
+        issues: []
       }
-    }
+      
+      componentDidMount() {
+        axios
+          .get("https://still-ridge-83597.herokuapp.com/users.json")
+          .then(response => {
+    
+            // create an array of contacts only with relevant data
+            const newUsers = response.data.map(c => {
+              return {
+                id: c.id,
+                name: c.name
+              };
+            });
+    
+            // create a new "State" object without mutating 
+            // the original State object. 
+            const newState = Object.assign({}, this.state, {
+              contacts: newUsers
+            });
+    
+            // store the new state object in the component's state
+            this.setState(newState);
+          })
+          .catch(error => console.log(error));
+     }
+      
+    render() {
+      return (
+        <div className="App">
 
-    export default App;
+          <UsersList issues={this.state.issues} />
+        </div>
+      );
+    }
+  }
+
+  export default App;
