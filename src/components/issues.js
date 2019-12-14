20685e73acb2d6f5fd79f203795d8d7df3c4dd77
Ubuntu <ubuntu@ip-172-31-25-147.us-east-2.compute.internal>
2019-12-14 13:38:16 +0000

    try API Get Issues

--- src/components/issues.js
+++ src/components/issues.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-    const Issues = ({ issues }) => {
+    /*const Issues = ({ issues }) => {
       return (
         <div>
-          <center><h1>Issues List</h1></center>
+          <center><h1>Issue Tracker</h1></center>
           {issues.map((issue) => (
             <div class="card">
               <div class="card-body">
@@ -12,6 +12,34 @@ import React from 'react'
               </div>
             </div>
           ))}
+        </div>
+      )
+    };*/
+    
+    const Issues = ({ issues }) => {
+      return (
+        <div>
+          <center><h1>Issue Tracker</h1></center>
+          <table>
+              <thead>
+                <tr>
+                    <th onclick="sortTable(0)">Title</th>
+                    <th onclick="sortTable(1)">T</th>
+                    <th onclick="sortTable(2)">P</th>
+                    <th onclick="sortTable(3)">Status</th>
+                    <th onclick="sortTable(4)">Votes</th>
+                    <th onclick="sortTable(5)">Assignee</th>
+                    <th onclick="sortTable(6)"> Created</th>
+                    <th onclick="sortTable(7)">Updated</th>
+                    <th colspan="1"></th>
+                 </tr>
+              </thead>
+              <tbody>
+                  {issues.map((issue) => (
+                    <td>{issue.title}</td>
+                  ))}
+              </tbody>
+          </table>
         </div>
       )
     };
