c9790034d22ca67bf1272f4d04c727bf86cd274e
Ubuntu <ubuntu@ip-172-31-25-147.us-east-2.compute.internal>
2019-12-14 11:51:10 +0000

    try API Get Users

--- src/App.js
+++ src/App.js
@@ -1,45 +1,28 @@
 import React, { Component } from 'react';
 import axios from "axios";
+import Users from './components/users';
+import fetch from 'isomorphic-fetch'
 
     class App extends Component {
       
       state = {
-        issues: []
+        users: []
+      }
+     
+     componentDidMount() {
+        fetch('https://still-ridge-83597.herokuapp.com/users.json')
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ users: data })
+        })
+        .catch(console.log)
       }
       
-      componentDidMount() {
-        axios
-          .get("https://still-ridge-83597.herokuapp.com/users.json")
-          .then(response => {
-    
-            // create an array of contacts only with relevant data
-            const newUsers = response.data.map(c => {
-              return {
-                id: c.id,
-                name: c.name
-              };
-            });
-    
-            // create a new "State" object without mutating 
-            // the original State object. 
-            const newState = Object.assign({}, this.state, {
-              contacts: newUsers
-            });
-    
-            // store the new state object in the component's state
-            this.setState(newState);
-          })
-          .catch(error => console.log(error));
-     }
-      
     render() {
-      return (
-        <div className="App">
-
-          <UsersList issues={this.state.issues} />
-        </div>
-      );
-    }
+        return (
+          <Users users={this.state.users} />
+        )
+      }
   }
 
-  export default App;
+  export default App
